a4c4900c02434f21bfcc1fe079a3cb925e6708b8
Scott Lillibridge <slillibridge@whitepages.com>
2014-09-18 14:36:50 -0700

    rsc_defaults, not rsc_default; version bump

--- providers/rsc_default.rb
+++ providers/rsc_default.rb
@@ -5,9 +5,9 @@ action :create do
     value = new_resource.value
 
     unless resource_exists?(name)
-        cmd = "crm configure rsc_default #{name}=\"#{value}\""
+        cmd = "crm configure rsc_defaults #{name}=\"#{value}\""
         Chef::Log.debug("configuring rsc_defaults #{cmd}")
-        e = execute "configure rsc_default #{name}" do
+        e = execute "configure rsc_defaults #{name}" do
             command cmd
         end
         new_resource.updated_by_last_action(true)
